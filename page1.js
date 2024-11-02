441702e4d6d6333b33bcda8a580acfaa8f018b29
Fernanda Barcellos <fernanda.barcellosbastos@gmail.com>
2024-11-01 23:34:57 -0300

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,17 @@
+const themToglle = document.getElementById("themeToggle");
+
+themeToggle.addEventlistener('change', () =>{
+    document.body.classList.toggle('dark', themeToggle.checked);
+})
+
+const abrir = document.querySelector("#abrir")
+const fechar = document.querySelector("#fechar")
+const modal = document.querySelector("#modal")
+
+abrir.addEventListener("click", ()=>{
+    modal.showModal()
+})
+
+fechar.addEventListener("click", ()=>{
+    modal.close()
+})
